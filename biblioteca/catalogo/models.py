b976042daa2b708e7e26a59c2ed4165486de5a3a
Henri Okayama <henri.okayama@gmail.com>
2024-11-14 11:20:59 -0300

    Definição da tabelas no catalogo

--- biblioteca/catalogo/models.py
+++ biblioteca/catalogo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    nacionalidade = models.CharField(max_length=50)
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=50)
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=100)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    genero = models.ForeignKey(Genero, on_delete=models.SET_NULL, null=True)
+    editora = models.ForeignKey(Editora, on_delete=models.SET_NULL, null=True)
+    ano_publicacao = models.IntegerField()
+
+class Emprestimo(models.Model):
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    data_emprestimo = models.DateField()
+    data_devolucao = models.DateField(null=True, blank=True)
